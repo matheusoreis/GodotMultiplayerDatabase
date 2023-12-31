985e0a107e13fd10c6121ff0fecc5fc825c8329f
Matheus Reis <47448206+matheusoreis@users.noreply.github.com>
2023-12-31 19:05:20 -0300

    Update - Correções do BootUI
    Botões genéricos agora trocados para os nomes corretos, organização geral e documentação

--- scripts/interface/boot_ui.gd
+++ scripts/interface/boot_ui.gd
@@ -4,20 +4,49 @@ extends Control
 @export var auth_panel: Control
 @export var server_ui: Control
 
-func _on_submit_sign_in_button_3_pressed():
+
+@export_category('Client Configuration')
+@export var host: String = '127.0.0.1'
+
+@export_category('Server Configuration')
+@export var port: int = 8082
+@export var max_clients: int = 10
+
+# Função chamada quando o botão de iniciar cliente é pressionado
+func _on_start_client_button_pressed() -> void:
+    # Cria um novo peer de multiplayer ENet
     var peer = ENetMultiplayerPeer.new()
-    peer.create_client('127.0.0.1', 8082)
+
+    # Configura o peer para se conectar a um servidor
+    peer.create_client(host, port)
+
+    # Define o peer de multiplayer para o peer criado
     multiplayer.multiplayer_peer = peer
+
+    # Altera a visibilidade dos painéis
     boot_panel.visible = false
     auth_panel.visible = true
     server_ui.visible = false
+
+    # Libera o nó da tree
     queue_free()
 
-func _on_submit_sign_in_button_4_pressed():
+
+# Função chamada quando o botão de iniciar servidor é pressionado
+func _on_start_server_button_pressed() -> void:
+    # Cria um novo peer de multiplayer ENet
     var peer = ENetMultiplayerPeer.new()
-    peer.create_server(8082, 5)
+
+    # Configura o peer para criar um servidor
+    peer.create_server(port, max_clients)
+
+    # Define o peer de multiplayer para o peer criado
     multiplayer.multiplayer_peer = peer
+
+    # Altera a visibilidade dos painéis
     boot_panel.visible = false
     auth_panel.visible = false
     server_ui.visible = true
+
+    # Libera o nó da tree
     queue_free()
